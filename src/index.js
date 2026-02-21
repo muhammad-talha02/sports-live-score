e8820728cbdfbbf0545cbac870b2d1faca17c454
Muhammad Talha Saeed <iamtalha971@gmail.com>
2026-02-21 12:36:29 +0400

    feat: implement arcjet for bot detection and rate limiting

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { matchRouter } from "./routes/matches.js";
 import http from "node:http";
 import attachWebSocketServer from "./ws/server.js";
+import { securityMiddleware } from "./config/arcjet.js";
 
 const PORT = process.env.PORT || 8000;
 const HOST = process.env.HOST || "0.0.0.0";
@@ -14,6 +15,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "Server is running" });
 });
+
+app.use(securityMiddleware());
 
 app.use("/matches", matchRouter);
 
--- src/ws/server.js
+++ src/ws/server.js
@@ -1,4 +1,5 @@
 import { WebSocket, WebSocketServer } from "ws";
+import { wsArcjet } from "../config/arcjet.js";
 
 function sendJson(socket, payload) {
   if (socket.readyState !== WebSocket.OPEN) return;
@@ -19,9 +20,28 @@ export default function attachWebSocketServer(server) {
     path: "/ws",
     maxPayload: 1024 * 1024, // 1MB
   });
-  wss.on("connection", (socket) => {
+  wss.on("connection", async (socket, req) => {
+    if (wsArcjet) {
+      try {
+        const decision = await wsArcjet.protect(req);
+        if (decision.isDenied()) {
+          const code = decision.reason.isRateLimit() ? 1013 : 1008;
+          const reason = decision.reason.isRateLimit()
+            ? "Rate Limit Exceeded"
+            : "Access isDenied";
+
+          socket.close(code, reason);
+
+          return;
+        }
+      } catch (error) {
+        console.log("WS Error Connection");
+        socket.close(1011, "Server Security Error");
+      }
+    }
+
     socket.isAlive = true;
-    
+
     socket.on("pong", () => {
       socket.isAlive = true;
     });
@@ -39,7 +59,7 @@ export default function attachWebSocketServer(server) {
     });
   }, 30000);
 
-  wss.on("close", () =>clearInterval(interval));
+  wss.on("close", () => clearInterval(interval));
   function broadcastMatchCreated(match) {
     broadcast(wss, { type: "match_created", data: match });
   }
